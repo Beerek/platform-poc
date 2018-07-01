40959ce811dd9d48bbdbc2b60d684e3626e947c0
ZheniaTrochun <zheniatrochun@gmail.com>
2018-07-01 17:08:16 +0300

    config files refactoring with tests

---
+++ http-gateway/src/test/java/com/beerek/platform/GatewayServerTest.java
@@ -0,0 +1,13 @@
+package com.beerek.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GatewayServerTest {
+    @Test
+    public void startupTest() { }
+}
